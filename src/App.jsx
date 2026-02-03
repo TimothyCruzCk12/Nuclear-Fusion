1ac2d18ebe69b70fb989828f7b45c03136870c6a
User <user@example.com>
2026-02-03 15:22:39 -0800

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NuclearFusion from './components/NuclearFusion'
+
+
+function App() {
+  return (
+      <div className="App">
+        <NuclearFusion />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/NuclearFusion.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const NuclearFusion = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default NuclearFusion;
